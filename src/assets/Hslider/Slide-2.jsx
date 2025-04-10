95a403b47a8f1061b1a8dc0fcc3a963a98a58f0b
monga1807 <smonga43732@gmail.com>
2025-04-10 12:14:29 +0530

    slider-2

--- src/assets/Hslider/Slide-2.jsx
+++ src/assets/Hslider/Slide-2.jsx
@@ -5,15 +5,15 @@ const Slide2 = () => {
     return (
       <div className='Slide2'>
         <h1>OLA</h1>
-        <div className='S1pro3-head'>Introducing</div>
-        <div className="S1pro3-img">
+        <div className='Slide2-head'>Introducing</div>
+        <div className="Slide2-img">
           <img src="https://cdn.olaelectric.com/sites/evdp/pages/home/home_banner_roadster_x.webp" alt="" />
         </div>
-        <div className='S1pro3-head-2'>
+        <div className='Slide2-head-2'>
           <h1>The Future of Motorcycling is here</h1>
         </div>
         <p>starting at an introductory price of ₹84,999</p>
-        <div className="S1pro3-buttons">
+        <div className="Slide2-buttons">
           <button className='buttons-b-b1'> Explore Now</button>
           <button className='buttons-b-b2'>Book Now </button>
         </div>
